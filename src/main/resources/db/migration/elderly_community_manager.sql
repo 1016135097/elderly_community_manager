43efe73a7fefe987dc61805e55e205795cec9767
ambitionkingman <864113210@qq.com>
2018-04-14 15:37:18 +0800

    更新表

--- src/main/resources/db/migration/elderly_community_manager.sql
+++ src/main/resources/db/migration/elderly_community_manager.sql
@@ -1,12 +1,12 @@
-DROP TABLE `user`;
-DROP TABLE `system_user`;
-DROP TABLE `residence`;
-DROP TABLE `retire`;
-DROP TABLE `medical_and_life`;
-DROP TABLE `children`;
-DROP TABLE `illness`;
-DROP TABLE `work_flow`;
-DROP TABLE `household`;
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `system_user`;
+DROP TABLE IF EXISTS `residence`;
+DROP TABLE IF EXISTS `retire`;
+DROP TABLE IF EXISTS `medical_and_life`;
+DROP TABLE IF EXISTS `children`;
+DROP TABLE IF EXISTS `illness`;
+DROP TABLE IF EXISTS `work_flow`;
+DROP TABLE IF EXISTS `household`;
 
 -- 用户表
 CREATE TABLE `user` (
@@ -20,7 +20,7 @@ CREATE TABLE `user` (
 `password` varchar(50) NULL COMMENT '密码',
 `type` int(2) NULL DEFAULT 0 COMMENT '类型：',
 `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-`id_number (`mobile`),
+ UNIQUE(`id_number`),
 PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
@@ -34,90 +34,111 @@ CREATE TABLE `system_user` (
 `id_number` varchar(20) NULL COMMENT '身份证号',
 `email` varchar(50) NULL COMMENT '邮箱',
 `password` varchar(50) NULL COMMENT '密码',
-`type` int(2) NULL DEFAULT 0 COMMENT '类型：0，操作员。1.管理员',
+`type` int(3) NULL COMMENT '用户类型',
 `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ UNIQUE(`id_number`),
 PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统用户表';
 
 -- 居住信息表
 CREATE TABLE `residence` (
 `id` bigint(20) NOT NULL AUTO_INCREMENT,
-`name` varchar(50) NULL COMMENT '名称',
+`user_id` bigint(20) NOT NULL COMMENT '用户id',
+`address` varchar(200) NULL COMMENT '地址',
+-- TODO
 `code` varchar(10) NULL COMMENT '代码',
 `location` varchar(255) NULL COMMENT '位置',
 PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='影院表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='居住信息表';
 
 -- 退休情况表
 CREATE TABLE `retire` (
 `id` bigint(20) NOT NULL AUTO_INCREMENT,
 `user_id` bigint(20) NOT NULL COMMENT '用户id',
-
-
+`age` int(4) NULL COMMENT '退休年龄',
+`type` int(3) NULL  COMMENT '退休类型',
+`retire_time` datetime NULL COMMENT '退休时间',
+-- TODO
 `name` varchar(50) NULL COMMENT '名称',
 `code` varchar(10) NULL COMMENT '代码',
-`type` int(2) NULL DEFAULT 0 COMMENT '类型：1.普通。2.2D。3.3D。4.Imax3D',
 `cinema_id` bigint(20) NULL COMMENT '影院id',
 PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='大厅表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='退休情况表';
 
 -- 医疗保险表
 CREATE TABLE `medical_and_life` (
 `id` bigint(20) NOT NULL AUTO_INCREMENT,
 `user_id` bigint(20) NULL COMMENT '用户id',
+`create_time` datetime NULL COMMENT '开始时间',
 
+-- TODO
 PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='座位表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='医疗保险表';
 
 -- 子女表
 CREATE TABLE `children` (
 `id` bigint(20) NOT NULL AUTO_INCREMENT,
-`user_id` bigint(20) NULL COMMENT '用户id
-',
-`code` varchar(10) NULL COMMENT '代码',
+`user_id` bigint(20) NULL COMMENT '用户id',
+`name` varchar(20) NULL COMMENT '用户名',
+`sex` int(2) NULL COMMENT '性别',
+`age` int(4) NULL COMMENT '年龄',
+`mobile` varchar(20) NULL COMMENT '手机号',
+`id_number` varchar(20) NULL COMMENT '身份证号',
+`email` varchar(50) NULL COMMENT '邮箱',
 `type` int(2) NULL COMMENT '类型',
-`year` timestamp NULL COMMENT '年代',
-`start_time` timestamp NULL COMMENT '上映时间',
-`end_time` timestamp NULL COMMENT '撤映时间',
-`area` varchar(255) NULL COMMENT '地区',
-`grade` numeric(5,0) NULL COMMENT '评分',
-`duration` numeric(5,0) NULL COMMENT '时长',
-`discounts` numeric(5,0) NULL COMMENT '优惠',
+`address` varchar(255) NULL COMMENT '住址',
+UNIQUE (`id_number`),
 PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='电影表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='子女表';
 
 -- 疾病情况表
 CREATE TABLE `illness` (
 `id` bigint(20) NOT NULL AUTO_INCREMENT,
-`login_name` varchar(20) NULL COMMENT '登录名',
-`user_name` varchar(20) NULL COMMENT '用户名',
-`sex` int(2) NULL COMMENT '性别',
-`mobile` varchar(20) NULL COMMENT '手机号',
-`email` varchar(50) NULL COMMENT '邮箱',
-`password` varchar(50) NULL COMMENT '密码',
-`type` int(2) NULL DEFAULT 0 COMMENT '类型：0，普通用户。1.管理员',
+`user_id` bigint(20) NULL COMMENT '用户id',
+`name` varchar(20) NULL COMMENT '疾病名称',
+`state` varchar(20) NULL COMMENT '疾病状态',
+`hospital` varchar(50) NULL COMMENT '主治医院',
+`type` int(2) NULL DEFAULT 0 COMMENT '类型',
+
+-- TODO
 PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='疾病情况表';
 
 -- 工作流程表
 CREATE TABLE `work_flow` (
 `id` bigint(20) NOT NULL AUTO_INCREMENT,
+`user_id` bigint(20) NULL COMMENT '用户id',
+`name` varchar(20) NULL COMMENT '名称',
+-- TODO
 `grade` numeric(5,0) NULL,
 `content` text NULL,
 `user_id` bigint(20) NULL,
 `movie_id` bigint(20) NULL,
 PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='评论表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='工作流程表';
+
+-- 流程项
+CREATE TABLE `work_flow_item` (
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+`flow_id` bigint(20) NULL COMMENT '流程id',
+`name` varchar(20) NULL COMMENT '名称',
+`desc` varchar(255) NULL COMMENT '描述',
+`start_time` datetime NOT NULL COMMENT '开始时间',
+`end_time` datetime NOT NULL COMMENT '结束时间',
+`type` int(3) NULL COMMENT '状态',
+`important` int(3) NULL COMMENT '重要程度',
+PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='流程项';
 
 -- 户籍情况表
 CREATE TABLE `household` (
 `id` bigint(20) NOT NULL AUTO_INCREMENT,
-`code` varchar(20) NULL COMMENT '序列号',
-`price` decimal(4,3) NULL COMMENT '价格',
-`move_id` bigint(20) NULL COMMENT '电影id',
-`create_time` timestamp NULL,
-`order_state` int(2) NULL COMMENT '订单状态。1，正常。0，退单',
-`pay_state` int(2) NULL COMMENT '支付状态。0，未支付；1.已支付',
-`disco_id` bigint(20) NULL COMMENT '优惠id',
+`user_id` bigint(20) NULL COMMENT '用户id',
+`master` varchar(20) NULL COMMENT '户主',
+`code` varchar(20) NULL COMMENT '户号',
+`address` varchar(255) NULL COMMENT '户籍地址',
+`type` int(3) NULL COMMENT '类型',
+
+-- TODO
 PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='户籍情况表';
